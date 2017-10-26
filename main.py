dd7bda52a6fe43e10763a96364c738fcade5449d
meilyasahsan <meilyasahsan@gmail.com>
2017-10-26 19:26:00 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import	tweepy
+from	textblob	import	TextBlob
+
+consumer_key	=	'<consumer_key>'
+consumer_secret	=	'<consumer_secret>'
+
+access_token	=	'<access_token>'
+access_token_secret	=	'<access_token_secret>'
+
+auth	=	tweepy.OAuthHandler(consumer_key,	consumer_secret)
+auth.set_access_token(access_token,	access_token_secret)
+
+api	=	tweepy.API(auth)
+
+public_tweets	=	api.search('hana anisa')
+
+for tweet in public_tweets:
+	print(tweet.text)
+	analysis	=	TextBlob(tweet.text)
+	print(analysis.sentiment)
+	pass
